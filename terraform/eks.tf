29710dff08d5029387122abfe88b784b1025e494
Ubuntu <ubuntu@ip-172-31-0-25.ap-south-1.compute.internal>
2026-09-07 20:15:12 +0000

    Sync pom.xml and eks.tf with actual deployed state

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -19,9 +19,9 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = [aws_subnet.public_1.id, aws_subnet.public_2.id]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
   }
 
   instance_types = ["t3.small"]
